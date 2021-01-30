220a14890b811a3cc90f99dcbe4734d2c61e2c55
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-30 13:34:50 +0000

    Change char format in  file 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for letter in range(122, 96, -1):
     if (letter % 2 == 0):
-        print("{}".format(chr(letter)), end="")
+        print("{:c}".format(letter), end="")
     else:
-        print("{}".format(chr(letter - 32)), end="
+        print("{:c}".format(letter - 32), end="")
