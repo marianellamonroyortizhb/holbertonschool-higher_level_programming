38b0e5541f45d41077df8e44653031a540cb6c81
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-30 13:25:48 +0000

    Add file 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for letter in range(122, 96, -1):
     if (letter % 2 == 0):
         print("{}".format(chr(letter)), end="")
     else:
-        print("{}".format(chr(letter - 32)), end="")
+        print("{}".format(chr(letter - 32)), end="
