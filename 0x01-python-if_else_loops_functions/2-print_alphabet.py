f52708184af5a4612007f69a77cd76be39cf1299
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-30 09:35:33 +0000

    Use format string to print just one letter per loop in file 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-alpha = ""
-for ind in range(97, 123):
-    alpha = alpha + chr(ind)
-print(str(alpha), end='')
+for alpha in range(97, 123):
+	print("{}".format(chr(alpha)), end="")
