94f178df4f81bf56ae705f8303479924697f4ae3
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 19:55:09 +0000

    Add file 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+        search_list = my_list.copy()
+        for ind in range(len(search_list)):
+                if (my_list[ind] == search):
+                        search_list[ind] = replace
+        return search_list
