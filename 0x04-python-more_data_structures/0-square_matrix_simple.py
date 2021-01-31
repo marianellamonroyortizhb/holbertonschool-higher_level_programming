c44a1ab476782a9b832df27adb255e9f49e059e1
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 19:16:40 +0000

    Add new line at the end in file 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,7 @@
 def square_matrix_simple(matrix=[]):
     square_mtx = matrix.copy()
     size = len(square_mtx)
-    for ind in range (size):
+    for ind in range(size):
         square_mtx[ind] = list(map(lambda a: a**2, square_mtx[ind]))
     return square_mtx
+
