ca7fd3dc0d6a4cd51f5013fbfe2c259b96543ea4
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 15:19:03 +0000

    Add file 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    my_list_cp = my_list.copy()
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        my_list_cp[idx] = element
+        return my_list_cp
