b06b4f453bef80d443ba2cdf27ed2cdc923dac80
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 21:24:17 +0000

    Add space before equal in file 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     double = {}
     for ind in a_dictionary:
-        double[ind]= 2 * a_dictionary[ind]
+        double[ind] = 2 * a_dictionary[ind]
     return double
