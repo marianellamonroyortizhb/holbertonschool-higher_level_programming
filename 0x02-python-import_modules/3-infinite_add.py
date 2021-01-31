d3ac39c18f9334dd15b109f9eb637d653bec3b93
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 13:27:21 +0000

    Add file 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argum = sys.argv
+    size = len(argum) - 1
+    accum = 0
+    for ind in range(1, size + 1):
+        accum = accum + int(argum[ind])
+    print("{}".format(accum))
