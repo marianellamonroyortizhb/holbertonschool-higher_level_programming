314746086a2ba639b2d47eb720875c953e0c0ed5
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 20:15:36 +0000

    Add file 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    diff_1 = set_1.difference(set_2)
+    diff_2 = set_2.difference(set_1)
+    sum_set = diff_1.union(diff_2)
+    return(sum_set)
