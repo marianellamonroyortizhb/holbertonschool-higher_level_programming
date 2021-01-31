3ad9be92b9a2ab8e9d0fbe54669ad549f6d79053
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 21:50:05 +0000

    Add file 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-void print_python_list(PyObject *p);
+def complex_delete(a_dictionary, value):
+    for n_key in list(a_dictionary.keys()):
+        if a_dictionary[n_key] == value:
+            del a_dictionary[n_key]
+    return a_dictionary
