8b75a169fdb00fb93b9d889652fb1ef4e6fcb0c5
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 20:01:28 +0000

    Add file 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    unique_list = (list(set(my_list)))
+    partial_sum = 0
+    for number in unique_list:
+        partial_sum = partial_sum + number
+    return(partial_sum)
