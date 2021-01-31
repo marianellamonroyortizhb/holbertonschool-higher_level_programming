3bd2bfa3cb40e1fab126d245a4a3810efade3caa
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 17:02:19 +0000

    Define a new list in file 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    my_list_cp = my_list
     if idx > 0:
-        del my_list[idx]
+        del my_list_cp[idx]
     else:
         pass
-    return (my_list)
+    return (my_list_cp)
