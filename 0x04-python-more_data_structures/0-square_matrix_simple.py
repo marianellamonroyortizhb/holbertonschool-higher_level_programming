c5d66086f50cdafdd9100a20993d3089b3c2341b
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 19:10:09 +0000

    Add file 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    square_mtx = matrix.copy()
+    size = len(square_mtx)
+    for ind in range (size):
+        square_mtx[ind] = list(map(lambda a: a**2, square_mtx[ind]))
+    return square_mtx
