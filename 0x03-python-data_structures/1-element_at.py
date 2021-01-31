8b213041a4f5dabd9c34b6422cfbacecb0d28fee
marianellamonroyortizhb <1880@holbertonschool.com>
2021-01-31 15:00:21 +0000

    Add file 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def element_at(my_list, inx):
+    if (inx < 0):
+        return None
+    elif(inx >= len(my_list)):
+        return None
+    else:
+        return(my_list[inx])
