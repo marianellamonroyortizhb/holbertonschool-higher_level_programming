8c1c01c2bbd8e72f3805dd6855793beadf5b5cd8
marianellamonroyortizhb <1880@holbertonschool.com>
2021-02-07 23:38:10 +0000

    Remove space before ( in file 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     ind = 0
-    for ind in range (x):
+    for ind in range(x):
         try:
             print("{}".format(my_list[ind]), end="")
             ind = ind + 1
