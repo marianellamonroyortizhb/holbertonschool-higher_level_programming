a5f368bf8ee429fe2018d34ad830840e5cb589d1
marianellamonroyortizhb <1880@holbertonschool.com>
2021-02-07 23:36:06 +0000

    Add file 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    ind = 0
+    for ind in range (x):
+        try:
+            print("{}".format(my_list[ind]), end="")
+            ind = ind + 1
+        except IndexError:
+            break
+    print("")
+    return(ind)
