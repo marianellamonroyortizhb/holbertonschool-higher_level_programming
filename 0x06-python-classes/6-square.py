0f5f917b2bf5754dc8e4db49f0af1e8674087e1f
marianellamonroyortizhb <1880@holbertonschool.com>
2021-02-08 03:43:41 +0000

    Add 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+"""Defining Square with size and area considering errors and using setter"""
+
+
+class Square:
+    """Square Class with size, area and errors definition"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        size_mult_twice = self.__size * self.__size
+        return size_mult_twice
+
+    @position.setter
+    def position(self, value):
+        if len(value) != 2 or type(value) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        if self.size != 0:
+            for count1 in range(0, self.size):
+                for count2 in range(0, self.size):
+                    print("#", end="")
+                print("")
+        else:
+            print("")
