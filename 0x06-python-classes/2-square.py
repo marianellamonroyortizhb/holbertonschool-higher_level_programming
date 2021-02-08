177b7c6f5af1885778fb9e1f144a0b6e5ce739fa
marianellamonroyortizhb <1880@holbertonschool.com>
2021-02-08 02:33:28 +0000

    Add file 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Defining Square with size considering errors"""
+
+
+class Square:
+    """Square Class with size and errors definition"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
