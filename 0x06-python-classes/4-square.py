6498e4474aa42126e9af9282cc7778ee135974f4
marianellamonroyortizhb <1880@holbertonschool.com>
2021-02-08 02:53:35 +0000

    Add 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Defining Square with size and area considering errors"""
+
+
+class Square:
+    """Square Class with size, area and errors definition"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        size_mult_twice = self.__size * self.__size
+        return size_mult_twice
+
+    @property
+    def size(self):
+            return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
