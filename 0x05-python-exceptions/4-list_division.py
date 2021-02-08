ae1b1c6e7a620c254bd7266fd0213060bf251dde
marianellamonroyortizhb <1880@holbertonschool.com>
2021-02-08 00:44:31 +0000

    Add ZeroDivisionError exception in file 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,14 +2,15 @@
 def list_division(my_list_1, my_list_2, list_length):
     final = []
     for ind in range(list_length):
+        div_list = 0
         try:
             div_list = my_list_1[ind] / my_list_2[ind]
         except IndexError:
-            div_list = 0
             print("out of range")
-        except ZeroDivisionError:
-            div_list = 0
+        except TypeError:
             print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
         finally:
             final.append(div_list)
     return final
