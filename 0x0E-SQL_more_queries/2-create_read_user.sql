f80fc1dae52fba76fd05757b3343e6b87d293391
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-05 04:53:03 +0000

    Remove 2 in file 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates an user
 -- Check if the user exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d2_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
