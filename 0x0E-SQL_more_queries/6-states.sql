0977293fbfd334dec974d91e287181375ece9b19
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-05 04:59:46 +0000

    Add files 6 and 7

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,1 +1,8 @@
-
+-- Creates datatable
+-- Creates datatable
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
