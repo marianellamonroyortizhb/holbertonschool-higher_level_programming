46aa40e5056b2b329e0f2769b423dba69612ecf7
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-15 00:22:45 +0000

    Add file 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
+def weight_average(my_list=[]):
+    if my_list:
+        operation = (sum(map(lambda x: x[0] * x[1], my_list)))
+        prom = (sum(map(lambda x: x[1], my_list)))
+        return (operation / prom)
+    else:
+        return 0
