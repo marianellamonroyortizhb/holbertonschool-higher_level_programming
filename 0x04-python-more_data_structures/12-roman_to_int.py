bc4d6a3edcc0a76a3e5da8661f37051144c5c13e
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-15 00:20:01 +0000

    Add file 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    if type(roman_string) != str or roman_string is None:
+        return 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i-1]]:
+            result += roman[roman_string[i]] - (roman[roman_string[i-1]] * 2)
+        else:
+            result += roman[roman_string[i]]
+    return result
