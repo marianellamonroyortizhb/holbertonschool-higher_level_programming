aa2327cb178c8e88ad4fdbb00542f5d2a4706873
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 01:24:16 +0000

    Change idantation to spaces in file 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const InputArg = process.argv;
 if (InputArg.length < 3) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (InputArg.length === 3){
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
