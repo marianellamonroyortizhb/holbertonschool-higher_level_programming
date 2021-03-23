e36b12f3a551f1c9c96225011d0db9aa0c59154a
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 04:42:48 +0000

    Add file 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
-let res;
+function add (a, b) {
+  console.log(a + b);
+}
 
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-function add (a, b) {
-  res = parseInt(arg1) + parseInt(arg2);
-  console.log(res);
-}
-
-add (arg1, arg2);
+add(parseInt(arg1, arg2);
