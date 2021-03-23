3be375d5e8950fd04ef1addcae2c64db5e739b33
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 01:54:54 +0000

    Remove variable in file 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const inputar = process.argv;
-if (inputar.length < 3) {
+if (process.argv.length < 3) {
   console.log('No argument');
-} else if (inputar.length === 3){
+} else if (process.argv.length === 3){
   console.log('Argument found');
 } else {
   console.log('Arguments found');
