adbb216946344bbb7203c6b1203e4722ac858399
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 03:39:07 +0000

    Export add function in file 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-let res;
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
 
-function add (a, b) {
-  res = parseInt(arg1) + parseInt(arg2);
-  console.log(res);
-}
-
-add (arg1, arg2);
+exports.add = function (a, b) {
+  return (a + b);
+};
