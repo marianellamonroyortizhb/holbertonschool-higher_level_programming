1b0316e0c47d795960371e0aa72a1f0bd9ac049c
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 03:19:48 +0000

    Change order or function in file 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-
 function add(a, b) {
   console.log(a + b);
 }
+
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
 
 add (arg1, arg2);
