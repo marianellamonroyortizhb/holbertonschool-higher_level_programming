39c7a240507d2c3d2dde8fe6f43887088396540f
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 02:32:43 +0000

    Change identation in file 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv[2] == null) {
-	console.log('Not a number');
-  } else if (parseInt(process.argv[2])) {
-	console.log('My number: ' + parseInt(process.argv[2]));
-  } else {
-	console.log('Not a number');
-  }
+  console.log('Not a number');
+} else if (parseInt(process.argv[2])) {
+  console.log('My number: ' + parseInt(process.argv[2]));
+} else {
+  console.log('Not a number');
+}
