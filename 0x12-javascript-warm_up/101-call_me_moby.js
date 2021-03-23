4441ae4bb9723bbfa7a5843f2d99da62b05c1a39
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 05:06:46 +0000

    Define let outsite the for in file 101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.callMeMoby = function (x, theFunction) {
-  let counter = 0;
-  for (counter; counter < x; counter++) {
-    TheFunction();
+  let count = 0;
+  for (count; count < x; count++) {
+    theFunction();
   }
 };
