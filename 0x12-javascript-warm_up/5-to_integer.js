bde6a47455b0eb17c4d30a864fef7c8c642fd6a5
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 02:29:44 +0000

    Add My Number in file 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 if (process.argv[2] == null) {
 	console.log('Not a number');
   } else if (parseInt(process.argv[2])) {
-	console.log(parseInt(process.argv[2]));
+	console.log('My number: ' + parseInt(process.argv[2]));
   } else {
 	console.log('Not a number');
   }
