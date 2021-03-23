23274c7c40d02ffde468ac7e98857923a41718f7
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 04:15:16 +0000

    Change again the function in file 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 
-exports.add = function (a, b) {
-  return (a + b);
+let res;
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+function add (a, b) {
+  res = parseInt(arg1) + parseInt(arg2);
+  console.log(res);
 }
+
+add (arg1, arg2);
