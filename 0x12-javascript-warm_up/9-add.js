4aa571291fe3113f20fb8a53efc4d3ef4065bf32
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 03:32:32 +0000

    Add let res in file 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-
+let res;
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
 function add (a, b) {
-  console.log(parseInt(arg1) + parseInt(arg2));
+  res = parseInt(arg1) + parseInt(arg2);
+  console.log(res);
 }
 
 add (arg1, arg2);
