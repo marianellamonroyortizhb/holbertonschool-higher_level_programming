e24b28770f1da10c2583f5a2ff959f92261d8a07
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 03:29:20 +0000

    Renove parseInt in const in in file 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-function add (a, b) {
-  console.log(a + b);
-}
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
+function add (a, b) {
+  console.log(parseInt(arg1) + parseInt(arg2));
+}
 
 add (arg1, arg2);
