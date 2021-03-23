dbbaa293bf932cd16e7607617fff7da21110ddba
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-23 01:20:57 +0000

    Remove let res and add console.log in each condition in file 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const InputArg = process.argv;
-let res;
 if (InputArg.length < 3) {
-	res = 'No argument';
+	console.log('No argument');
 } else if (InputArg.length === 3){
-	res = 'Argument found';
+	console.log('Argument found');
 } else {
-	res = 'Arguments found';
+	console.log('Arguments found');
 }
-console.log(res);
