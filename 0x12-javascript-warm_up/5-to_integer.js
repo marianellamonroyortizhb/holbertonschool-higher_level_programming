7c9f2d511508add5b25714f244c7eb7b770dc66e
marianellamonroyortizhb <1880@holbertonschool.com>
2021-08-03 02:47:09 +0000

    Remove second line in file 5-multi_languages_loop.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ if (process.argv[2] == null) {
 } else if (parseInt(process.argv[2])) {
   console.log('My number: ' + parseInt(process.argv[2]));
 } else {
-  console.log('Not a number');
+  console.log('Not a number');}
 }
