3ed93a69be51a1ee73faf20bccf06e9960d3d0a9
marianellamonroyortizhb <1880@holbertonschool.com>
2021-08-03 03:37:40 +0000

    Remove space in file 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ let mult;
 
 if (parseInt(process.argv[2])) {
   for (mult = 1; mult <= process.argv[2]; mult++) {
-    console.log('C is Fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
